f8d7f9c4219d180b139e8cb6b183f18d82393d74
Kashish Sharma <s.kashishsharma06@gmail.com>
2026-04-30 12:26:41 +0530

    fixed register serializer for frontend

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -4,11 +4,21 @@ from .models import User
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
+    name = serializers.CharField(required=False)
+    organization = serializers.CharField(required=False)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ['username', 'name', 'organization', 'email', 'password']
 
     def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
+        validated_data.pop('name', None)
+        validated_data.pop('organization', None)
+
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+
         return user
